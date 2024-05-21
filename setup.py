4c5c9aec05c6d1da351529c7b28e94765ef5b4d8
ShanderYO <ShanderYO@gmail.com>
2024-05-21 03:12:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,21 +8,6 @@ setup(
     author='getvalet',
     author_email='ваша_почта@example.com',
     description='Сохраненная версия пакета',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://gitlab.com/getvalet/aiogram-broadcaster-0.0.7',
-    packages=['aiogram_broadcaster'],  # Пример, как указать конкретные пакеты
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Natural Language :: Russian',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.7',
-    install_requires=[
-        # Сюда можно добавить зависимости, если они есть
-    ],
+    url='https://github.com/ShanderYO/aiogram-broadcaster-0.0.7-save/',
+    packages=find_packages()
 )
